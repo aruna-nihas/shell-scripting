aee00202a8c7a567e0d3535c034ac393976ce7b6
aruna <arunadegala18@gmail.com>
2023-12-15 15:53:06 +0530

    changed

---
+++ 07.array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango" "Grapes")
+echo "First fruis is:${FRUITS[0]}"
+echo "second fruis is:${FRUITS[1]}"
+echo "Third fruis is:${FRUITS[2]}"
+echo "fourth fruis is:${FRUITS[3]}"
+echo "All fruis are:${FRUITS[@]}"
+echo "No of fruis:${FRUITS[#]}"
