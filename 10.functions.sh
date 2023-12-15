57baea0ad8b86702fb11cab14ce4567456ad9f2b
aruna <arunadegala18@gmail.com>
2023-12-15 17:07:38 +0530

    installing mysql

--- 10.functions.sh
+++ 10.functions.sh
@@ -5,14 +5,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 VALIDATE(){
-    if [ $1 -ne 0 ]
-then
-    echo -e "$R $2 is Failed....$N"
-    exit 1
-else
-     
-   echo -e "$G $2 successfully....$N"
-fi
+        if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2 is Failed....$N"
+        exit 1
+    else
+        
+    echo -e "$G $2 successfully....$N"
+    fi
     
 }
 if [ $ID -ne 0 ]
---
+++ 11.logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$R $2 is Failed....$N"
+    exit 1
+else
+     
+   echo -e "$G $2 successful....$N"
+fi
+}
+if [ $1 -ne 0 ]
+then
+    echo -e "$R ERROR::PLease use Root user $N"
+    exit 1
+else 
+        echo -e "$Y U R root user"
+fi
+yum install mysql -y
+VALIDATE $? "MySQL Installing" &>> $LOGFILE
+yum install git -y
+VALIDATE $? "GIT Installing" &>> $LOGFILE
