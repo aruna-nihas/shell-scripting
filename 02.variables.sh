3bfc7e32312a3a5fd2a376db0e73b4633be08d48
aruna <arunadegala18@gmail.com>
2023-12-15 15:02:20 +0530

    comment

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+PERSON1=Suresh
+PERSON2=Ramesh
 
-PERSON1=Ramesh
-PERSON2=Suresh
-
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you"
+echo " $PERSON1: Hi $PERSON2,Good Morning"
+echo " $PERSON2: Hello $PERSON1,very good morning"
+echo " $PERSON1: How are you $PERSON2 ?"
+echo " $PERSON2: Iam good $PERSON1,how r u?"
