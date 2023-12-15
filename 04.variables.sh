8ce9f4cb5d1f068745651195ae6206e1c09f4b5c
aruna <arunadegala18@gmail.com>
2023-12-15 15:20:23 +0530

    changed

---
+++ 04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo " $PERSON1: Hi $PERSON2,Good Morning"
+echo " $PERSON2: Hello $PERSON1,very good morning"
+echo " $PERSON1: How are you $PERSON2 ?"
+echo " $PERSON2: Iam good $PERSON1,how r u?"
